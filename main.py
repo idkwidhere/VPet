67e99a5506af5adc8212acbde5b485ae4b1eb71a
Aaron Johnson <adjgamer321@gmail.com>
2023-08-03 10:04:03 -0400

    added gameclock and mon stages

--- main.py
+++ main.py
@@ -23,6 +23,75 @@ def draw_text(text, font, text_color, x, y):
     img = font.render(text, True, text_color)
     screen.blit(img, (x, y))
 
+#########################MONS##########################
+from random import randint
+
+class Egg():
+    def __init__(self):
+        self.lifespan = 15
+        self.stage = 0
+
+class Baby():
+    def __init__(self):
+        self.lifespan = 15
+        self.stage = 1
+        self.happiness = 0
+        self.max_happiness = 10
+        self.hunger = 0
+        self.max_hunger = 10
+        if randint(0, 100) > 50:
+            self.sex = "Male"
+        else:
+            self.sex = "Female"
+        self.items = ["rattle"]
+
+class Toddler():
+    def __init__(self, sex, items):
+        self.lifespan = 15
+        self.stage = 2
+        self.happiness = 0
+        self.max_happiness = 10
+        self.hunger = 0
+        self.max_hunger = 10
+        self.sex = sex
+        self.items = items
+
+class Teen():
+    def __init__(self, sex, items):
+        self.lifespan = 15 
+        self.stage = 3
+        self.happiness = 0
+        self.max_happiness = 10
+        self.hunger = 0
+        self.max_hunger = 10
+        self.sex = sex
+        self.items = items
+
+class Adult():
+    def __init__(self, sex, items):
+        self.lifespan = 15
+        self.stage = 4
+        self.happiness = 0
+        self.max_happiness = 10
+        self.hunger = 0
+        self.max_hunger = 10
+        self.sex = sex
+        self.items = items
+
+class Elder():
+    def __init__(self, sex, items):
+        self.lifespan = 15
+        self.stage = 5
+        self.happiness = 0
+        self.max_happiness = 10
+        self.hunger = 0
+        self.max_hunger = 10
+        self.sex = sex
+        self.items = items
+
+#########################ENDMONS#######################
+
+
 ######################BUTTONS######################
 #button images
 status = pygame.image.load("./assets/buttons/stats.png")
@@ -53,7 +122,7 @@ class Button():
             screen.blit(self.image_hover, (self.rect.x, self.rect.y))
             if(pygame.mouse.get_pressed()[0] == 1 and self.clicked == False):
                 self.clicked = True
-                pygame.mixer.Sound.play(button_sound)
+                #pygame.mixer.Sound.play(button_sound)
                 #action
 
             if(pygame.mouse.get_pressed()[0] == 0):
@@ -73,6 +142,36 @@ shop_button = Button(84, 162, shop, shop_hover, action="show_shop")
 #sounds/music
 button_sound = pygame.mixer.Sound('./assets/sounds/buttons.wav')
 
+#hatching new egg
+def hatch(stage):
+    if stage == None:
+        print("You hatched a new egg!")
+        newmon = Egg()
+        return newmon
+    
+def evolve(stage):
+    match stage:
+        case 0:
+            print("evolve to a baby")
+            return Baby()
+        case 1:
+            print("evolve to a toddler")
+            return Toddler(mymon.sex, mymon.items)
+        case 2:
+            print("evolve to a teen")
+            return Teen(mymon.sex, mymon.items)
+        case 3:
+            print("evolve to an adult")
+            return Adult(mymon.sex, mymon.items)
+        case 4:
+            print("evolve to an elder")
+            return Elder(mymon.sex, mymon.items)
+        case 5:
+            print("You died!")
+            running = False
+
+
+stage = None
 
 running = True
 while running:
@@ -85,13 +184,33 @@ while running:
     eat_button.draw()
     shop_button.draw()
 
+
+    #age/hatch checks
+    if stage == None:
+            print("You hatched a new egg!")
+            mymon = Egg()
+            stage = mymon.stage
+            pygame.time.set_timer(pygame.USEREVENT, 1000)
+            cycle = mymon.lifespan
+
+    
+
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.USEREVENT:
+                cycle -= 1
+                print(cycle)
 
-    #check for age
+                if cycle <= 0:
+                    mymon = evolve(mymon.stage)
+                    print(mymon.sex, mymon.items)
+                    cycle = mymon.lifespan
+                        
 
+    #print(cycle)
+    #print(stage)
 
     pygame.display.update()
     clock.tick(30)
